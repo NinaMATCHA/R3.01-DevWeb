89092a2e50614d5aa47527ef1ce8d602b4469088
Nina MATIC CHARBIT <nina.MATIC-CHARBIT@etu.univ-amu.fr>
2026-09-22 21:47:28 +0000

    ajout des fonctionnalites demmandées des controlleurs (getConnection et getInstance) dans le database.php puis ajout de la base des modeles sur lesquels on se basera en reprenant exactement le CM4 + explications pr ceux qui sont perdus
    ATTENTION la connection n'a pas encore été testée donc ne mergez pas la branche

--- _assets/includes/database.php
+++ _assets/includes/database.php
@@ -1,17 +1,34 @@
-<?php 
+<?php
 
-function databaseConnection() {
-   try {
-    $dsn = 'mysql:host=mysql-ninamc.alwaysdata.net;dbname=ninamc_bdd';
-    $pdo = new \PDO($dsn, 'ninamc','NevotLe+Fortdu13');
-    $pdo->exec('SET CHARACTER SET utf8');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    return $pdo;
-    } catch (Exception $e) {
-        die('Connection impossible : ' . $e->getMessage());
+class DatabaseConnection
+{
+    private ?PDO $connection = null; # connection prsk on appelle getConnection dans le cours : c'est ce qui contient les param de la pdo
+    private static ?DatabaseConnection $instance = null; # instance prsk on appelle getInstance aussi : ça permet de rester sur la meme session
+
+    private function __construct()
+    {
+        try { # ça c comme dans le cours, hesitez pas a vous co avec les id pour voir la bdd
+            $dsn = 'mysql:host=mysql-ninamc.alwaysdata.net;dbname=ninamc_bdd';
+            $this->connection = new PDO($dsn, 'ninamc', 'NevotLe+Fortdu13');
+            $this->connection->exec('SET CHARACTER SET utf8');
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('Erreur de connexion : ' . $e->getMessage());
+        }
     }
 
-}
+    #vérifie si l'instance ( donc l'objet et la connexion en cours ) est null pour en creer une auto dans le controller soit renvoyer celle courante
+    public static function getInstance(): DatabaseConnection
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
 
-?>
+    #renvoie juste la co courante qui peut etre nulle sans __construct
+    public function getConnection(): PDO
+    {
+        return $this->connection;
+    }
+}
--- modules/models/Connection_model.php
+++ modules/models/Connection_model.php
@@ -2,21 +2,21 @@
 
 namespace modules\models;
 class Connection_model {
-
+    #appelle le construct du database.php et recupere ses id de connection pour pouvoir faire une query
     public function __construct(private \Includes\Database\DatabaseConnection $connection) {}
 
-    public function getConnection(): array
+    #retourne les resultats que l'on veut de notre query ( je verrai plus tard mais faudra comparer les login avc WHERE )
+    public function getConnection(String $login): array
     {
-        if (!$statement = $this->connection->getConnection()->query('SELECT id, title, content,
-        creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5’))
+        if (!$statement = $this->connection->getConnection()->query('SELECT login, password FROM users'))#<-- query ici [a changer c une base]
         {
             throw new DatabaseException();
         }
-        $posts = [];
-        while ($row = $statement->fetch(PDO::FETCH_OBJ)) {
-            $post = new Post($row->id, $row->title, $row->creation_date, $row->content);
-            $posts[] = $post;
+        $connection = [];
+        # ici ce qu'on veut faire avec ce que retourne notre query
+        while ($row = $statement->fetch(PDO::FETCH_OBJ)) { #fetch envoie false quand y'a plus r a envoyer
+            $connection[] = $row;
         }
-        return $posts;
+        return $connection;
     }
 }
