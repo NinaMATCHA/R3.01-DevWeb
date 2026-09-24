d27a601139745ba1c6382288095d15723b05cb60
Nicolas Moyenin <nicolas.moyenin@laposte.net>
2026-09-24 14:21:22 +0000

    fix/Ajout de fonction pour enregister et vérifier le code pour reset le mdp

--- modules/models/model_fp.php
+++ modules/models/model_fp.php
@@ -1,11 +1,31 @@
 <?php
-namespace modules\models; // PSR-12: head blocks must be separated by a single blank line
-class PostRepository { // PSR-12: opening brace next line
-    public function __construct(private \Includes\Database\DatabaseConnection $connection) {}
+namespace modules\models; 
+use PDO;
+class forgot_password_model {
+
+    //Fonction pour sauvegarder le code a usage unique.
+    private function saveCodeInDataBase(string $email, string $code): bool 
+    {
+        $pdo = \DatabaseConnection::getInstance()->getConnection();
+        $sql = "UPDATE users SET reset_code = :code WHERE email = :email";
+        $stmt = $pdo->prepare($sql);
+
+        return $stmt -> execute([
+            ':code'=>$code,
+            ':email'=>$email
+        ]);
+    }
 
     public function sendMail(string $to): bool
     {
     $code = (string) random_int(100000, 999999);
+    
+    //on verif si y'a pas de probleme pour eviter d'enregistrer des données non valides dans la BDD (genre mail manquant ou faux)
+    if (!$this -> saveCodeInDataBase($to, $code)){
+        return false;
+    }
+
+
     $subject = 'Code de récupération de mot de passe';
     $message = "Bonjour, voici votre code de récupération de mot de passe" . $code;
 
@@ -20,4 +40,22 @@ class PostRepository { // PSR-12: opening brace next line
     // Envoi
     return mail($to, $subject, $message, $headers);
 }
+
+    public function verifyCode(string $email, string $code, string $newPassWord){
+        $pdo = \DatabaseConnection::getInstance()->getConnection();
+        $sql = "SELECT id FROM users WHERE email = :email AND reset_code = :code";
+        $stmt = $pdo -> prepare($sql);
+        $stmt -> execute([':email' => $email, ':code' => $code]);
+
+        if ($stmt ->fetch()){
+            $passwordHash = password_hash($newPassWord, PASSWORD_DEFAULT);
+            $updateSql = "UPDATE users SET mdp = :mdp, reset_code = NULL WHERE email = :email";
+            $updateStmt = $pdo -> prepare($updateSql);
+
+            return $updateStmt -> execute([':mdp' => $passwordHash,':email' => $email]);
+        }
+        return false;
+    }
+
+
 }
